52063e10b40009943cae9786f63511e6620aea20
ezterry <ezterry3@gmail.com>
2011-04-10 21:43:59 -0400

    Test dalvik-vm Jit with AOSP armv5te over CM armv6j
    This will only override the Arch Variant for the Dalvik-VM
    the rest of the build remains armv6j incl. compiler optimizations
    on the dalvik VM

--- device_dream_sapphire.mk
+++ device_dream_sapphire.mk
@@ -90,6 +90,7 @@ JS_ENGINE := v8
 
 #use armv6j code
 TARGET_ARCH_VARIANT := armv6j
+TARGET_DVM_ARCH_VARIANT := armv5te
 
 # proprietary side of the device
 $(call inherit-product-if-exists, vendor/htc/dream-sapphire/device_dream_sapphire-vendor.mk)
